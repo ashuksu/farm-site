f40cf26202c63e55c3d707557ba1d89697a83fba
ashuksu <ashuksu@gmail.com>
2026-03-19 19:19:38 +0100

    refactor layout and styling for improved responsiveness and readability

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // ====== DATA ======
 const products = [
-    { id: 1, name: "Помидоры", price: 120, img: "https://images.unsplash.com/photo-1561136594-7f68413baa99" },
-    { id: 2, name: "Картофель", price: 60, img: "https://images.unsplash.com/photo-1698505949250-51f8b2c9c8c6" },
-    { id: 3, name: "Молоко", price: 90, img: "https://images.unsplash.com/photo-1632200823229-376320621350" },
-    { id: 4, name: "Яйца", price: 150, img: "https://images.unsplash.com/photo-1587486913049-53fc88980cfc" },
-    { id: 5, name: "Мёд", price: 300, img: "https://images.unsplash.com/photo-1613548058193-1cd24c1bebcf" },
-    { id: 6, name: "Сыр", price: 250, img: "https://images.unsplash.com/photo-1559561853-08451507cbe7" }
+    {id: 1, name: "Помидоры", price: 120, img: "https://images.unsplash.com/photo-1561136594-7f68413baa99"},
+    {id: 2, name: "Картофель", price: 60, img: "https://images.unsplash.com/photo-1698505949250-51f8b2c9c8c6"},
+    {id: 3, name: "Молоко", price: 90, img: "https://images.unsplash.com/photo-1632200823229-376320621350"},
+    {id: 4, name: "Яйца", price: 150, img: "https://images.unsplash.com/photo-1587486913049-53fc88980cfc"},
+    {id: 5, name: "Мёд", price: 300, img: "https://images.unsplash.com/photo-1613548058193-1cd24c1bebcf"},
+    {id: 6, name: "Сыр", price: 250, img: "https://images.unsplash.com/photo-1559561853-08451507cbe7"}
 ];
 
 let cart = [];
@@ -75,7 +75,7 @@ function addToCart(id) {
         existing.qty++;
     } else {
         const product = products.find(p => p.id === id);
-        cart.push({ ...product, qty: 1 });
+        cart.push({...product, qty: 1});
     }
 
     updateCart();
